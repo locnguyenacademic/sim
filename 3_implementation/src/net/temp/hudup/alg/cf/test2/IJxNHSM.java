c6de1d5bbeca2f2c2cdbe1f350f580c7812372b7
Loc Nguyen <ngphloc@gmail.com>
2021-04-10 21:14:23 +0700

    Update classes.

---
+++ 3_implementation/src/net/temp/hudup/alg/cf/test2/IJxNHSM.java
@@ -0,0 +1,103 @@
+/**
+ * SIM: MACHINE LEARNING ALGORITHMS FRAMEWORK
+ * (C) Copyright by Loc Nguyen's Academic Network
+ * Project homepage: sim.locnguyen.net
+ * Email: ng_phloc@yahoo.com
+ * Phone: +84-975250362
+ */
+package net.temp.hudup.alg.cf.test2;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.hudup.alg.cf.nb.NeighborCFExtUserBased;
+import net.hudup.core.data.Profile;
+import net.hudup.core.data.RatingVector;
+import net.hudup.core.logistic.ForTest;
+
+/**
+ * IJ + NHSM measure.
+ * 
+ * @author Loc Nguyen
+ * @version 1.0
+ *
+ */
+public class IJxNHSM extends NeighborCFExtUserBased implements ForTest {
+
+	
+	/**
+	 * Serial version UID for serializable class. 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	/**
+	 * Default constructor.
+	 */
+	public IJxNHSM() {
+
+	}
+
+
+	@Override
+	public List<String> getAllMeasures() {
+		return getMainMeasures();
+	}
+
+
+	@Override
+	public List<String> getMainMeasures() {
+		return Arrays.asList(getDefaultMeasure());
+	}
+
+
+	@Override
+	protected String getDefaultMeasure() {
+		return "a03.03.05.02.ijxnhsm";
+	}
+
+
+	@Override
+	public String getMeasure() {
+		return getDefaultMeasure();
+	}
+
+
+	@Override
+	protected void updateConfig(String measure) {
+		super.updateConfig(measure);
+		
+		config.remove(MEASURE);
+		config.remove(VALUE_BINS_FIELD);
+		config.remove(COSINE_NORMALIZED_FIELD);
+		config.remove(MSD_FRACTION_FIELD);
+		config.remove(BCF_MEDIAN_MODE_FIELD);
+		config.remove(MU_ALPHA_FIELD);
+		config.remove(SMTP_LAMBDA_FIELD);
+		config.remove(SMTP_GENERAL_VAR_FIELD);
+		config.remove(TA_NORMALIZED_FIELD);
+		config.remove(RATINGJ_THRESHOLD_FIELD);
+		config.remove(INDEXEDJ_INTERVALS_FIELD);
+	}
+
+
+	@Override
+	protected double sim0(String measure, RatingVector vRating1, RatingVector vRating2, Profile profile1,
+			Profile profile2, Object... params) {
+		double urp = urp(vRating1, vRating2, profile1, profile2);
+		double amert = amer(vRating1, vRating2, profile1, profile2);
+		return improvedJaccard(vRating1, vRating2, profile1, profile2) * amert * urp;
+	}
+
+	
+	@Override
+	public String getName() {
+		String name = getConfig().getAsString(DUPLICATED_ALG_NAME_FIELD);
+		if (name != null && !name.isEmpty())
+			return name;
+		else
+			return "a03.03.05.02.ijxnhsm";
+	}
+
+
+}
