9a5cdd34297e9dc8d7b692d1d82887468ac9f920
Loc Nguyen <ngphloc@gmail.com>
2021-06-24 20:15:43 +0700

    Update classes.

---
+++ 3_implementation/src/net/temp/hudup/alg/cf/test2/AmerxESim.java
@@ -0,0 +1,57 @@
+/**
+ * SIM: MACHINE LEARNING ALGORITHMS FRAMEWORK
+ * (C) Copyright by Loc Nguyen's Academic Network
+ * Project homepage: sim.locnguyen.net
+ * Email: ng_phloc@yahoo.com
+ * Phone: +84-975250362
+ */
+package net.temp.hudup.alg.cf.test2;
+
+import net.hudup.core.data.DataConfig;
+import net.hudup.core.logistic.ForTest;
+
+/**
+ * Amer + ESim measure.
+ * 
+ * @author Loc Nguyen
+ * @version 1.0
+ *
+ */
+public class AmerxESim extends net.hudup.alg.cf.nb.beans.smd.AmerxESim implements ForTest {
+
+	
+	/**
+	 * Serial version UID for serializable class. 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	/**
+	 * Default constructor.
+	 */
+	public AmerxESim() {
+
+	}
+
+
+	@Override
+	public String getName() {
+		String name = getConfig().getAsString(DUPLICATED_ALG_NAME_FIELD);
+		if (name != null && !name.isEmpty())
+			return name;
+		else
+			return "a22.01.23.01.amerxesim";
+	}
+
+
+	@Override
+	public DataConfig createDefaultConfig() {
+		DataConfig config = super.createDefaultConfig();
+		config.put(KNN, 100);
+		config.put(FAST_RECOMMEND, true);
+		
+		return config;
+	}
+
+
+}
