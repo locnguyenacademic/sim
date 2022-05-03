535038ceef2624a4d6c91c9d50aa7fdc4f88fa74
Loc Nguyen <ngphloc@gmail.com>
2022-05-03 16:43:10 +0700

    Update Hudup lib.

--- 3_implementation/src/net/jsi/UniverseRemoteGetter.java
+++ 3_implementation/src/net/jsi/UniverseRemoteGetter.java
@@ -6,6 +6,13 @@ import java.rmi.RemoteException;
 public interface UniverseRemoteGetter extends Remote {
 	
 
+	/**
+	 * Getting remote universe.
+	 * @param account specified account.
+	 * @param passwords specified password.
+	 * @return remote universe.
+	 * @throws RemoteException if any error raises.
+	 */
 	UniverseRemote getUniverseRemote(String account, String passwords) throws RemoteException;
 
 
